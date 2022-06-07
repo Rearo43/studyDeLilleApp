bb5586ac6637f8769bf80c4658f9720e28415405
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2022-06-07 04:30:28 -0700

    app up and running, installed first packages

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { HomeScreen } from './Home';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => (
+  <>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <SafeAreaProvider>
+        <HomeScreen />
+      </SafeAreaProvider>
+    </ApplicationProvider>
+  </>
+);
