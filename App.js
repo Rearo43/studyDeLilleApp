39ff2bad28679a8a7129baaebcf96be1a703a916
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2022-06-07 04:46:39 -0700

    cards working as expected

--- App.js
+++ App.js
@@ -3,12 +3,13 @@ import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { HomeScreen } from './Home';
+import { Wines } from './Wines';
 
 export default () => (
   <>
     <ApplicationProvider {...eva} theme={eva.light}>
       <SafeAreaProvider>
-        <HomeScreen />
+        <Wines />
       </SafeAreaProvider>
     </ApplicationProvider>
   </>
